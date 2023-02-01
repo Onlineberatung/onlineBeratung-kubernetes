81ce205a7419add23c0d6d78e584d29eb5b634a5
Marc Itzenthaler <itzenthaler@web-mi.de>
2023-02-01 22:46:54 +0100

    feat: activated more jitsi features

--- customization/jitsi-resources/web/custom-config.js
+++ customization/jitsi-resources/web/custom-config.js
@@ -6,13 +6,24 @@ config.enableLobbyChat = false;
 config.defaultLanguage = 'de';
 config.enableEncodedTransformSupport = true;
 config.toolbarButtons = [
-    'microphone',
     'camera',
+    //'chat', -> Disabled no e2ee support
+    //'etherpad', -> Disabled no e2ee support
+    'filmstrip',
+    'fullscreen',
     'hangup',
-    'desktop',
-    'settings',
+    'microphone',
+    'noisesuppression',
+    'participants-pane',
+    'raisehand',
     'select-background',
-    //'tileview'
+    'settings',
+    'shortcuts',
+    'tileview',
+    'toggle-camera',
+    'videoquality',
+    //'whiteboard', -> Disabled no e2ee support
+
 ];
 config.defaultLocalDisplayName = 'Ich';
 config.defaultRemoteDisplayName = 'Ratsuchender';
@@ -33,6 +44,32 @@ config.disabledSounds = [
     'E2EE_OFF_SOUND',
     'E2EE_ON_SOUND'
 ];
-config.channelLastN = 5;
 config.hideConferenceSubject = true;
 config.dynamicBrandingUrl = '/dynamicBranding.json';
+config.channelLastN = -1;
+config.lastNLimits = {
+    5: 20,
+    30: 15,
+    50: 10,
+    70: 5,
+    90: 2
+};
+config.enableIceRestart = true;
+config.giphy = {
+    enabled: false,
+};
+config.whiteboard = {
+    enabled: false,
+};
+config.gravatar = {
+    disabled: true,
+};
+config.breakoutRooms = {
+    hideAddRoomButton: true,
+    hideAutoAssignButton: true,
+    hideJoinRoomButton: true,
+    hideModeratorSettingsTab: true,
+    hideMoreActionsButton: true,
+    hideMuteAllButton: true,
+};
+config.disableThirdPartyRequests = true;
